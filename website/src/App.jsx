81e16ea08358338abf6c92c02017a20efcb15832
Dani Jourdain <danijourdain@icloud.com>
2026-02-18 18:12:11 -0700

    update readme * background color

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
 import Header from './components/Header'
 
