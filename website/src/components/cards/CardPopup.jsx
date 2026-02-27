11016ae301e2fb5524edbf15f6993138405e57d3
Dani Jourdain <danijourdain@icloud.com>
2026-02-26 17:39:49 -0700

    extract add button to component

--- website/src/components/cards/CardPopup.jsx
+++ website/src/components/cards/CardPopup.jsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
 import styles from './CardPopup.module.css';
+import AddButton from '../AddButton';
 
 const CardPopup = ({ isOpen, onClose, card }) => {
     return <>
@@ -17,26 +18,28 @@ const CardPopup = ({ isOpen, onClose, card }) => {
                         <h2>{card.name}</h2>
                         <button onClick={onClose} className={styles['close-button']}>&times;</button>
                     </div>
-                    <h3>{card.type} Card</h3>
-                    {card.type.toLowerCase() === 'ranged' && (
-                        <>
-                            <p><b>Range type:</b> {card.range_type}</p>
-                            <p><b>Range:</b> {card.min_range}-{card.max_range}</p>
+                    <div className={styles['card-content']}>
+                        <h3>{card.type} Card</h3>
+                        {card.type.toLowerCase() === 'ranged' && (
+                            <>
+                                <p><b>Range type:</b> {card.range_type}</p>
+                                <p><b>Range:</b> {card.min_range}-{card.max_range}</p>
 
-                        </>
-                    )}
-                    {(card.type.toLowerCase() === 'ranged' || card.type.toLowerCase() === 'melee') && (
-                        <>
-                            <p><b>Dice:</b> {card.dice}</p>
-                            <p><b>Damage:</b> {card.damage}</p>
-                            <p><b>Success:</b> {card.success}</p>
-                            <p><b>Dual wield:</b> {String(card.dual_wield)}</p>
-                            <p><b>Open Door:</b> {String(card.open_door)}</p>
-                        </>
-                    )}
-                    <p>{card.text}</p>
-                    <div className={styles['add-button']}>
-                        <button >Add</button>
+                            </>
+                        )}
+                        {(card.type.toLowerCase() === 'ranged' || card.type.toLowerCase() === 'melee') && (
+                            <>
+                                <p><b>Dice:</b> {card.dice}</p>
+                                <p><b>Damage:</b> {card.damage}</p>
+                                <p><b>Success:</b> {card.success}</p>
+                                <p><b>Dual wield:</b> {String(card.dual_wield)}</p>
+                                <p><b>Open Door:</b> {String(card.open_door)}</p>
+                            </>
+                        )}
+                        <p>{card.text}</p>
+                    </div>
+                    <div className={styles['add-button-container']}>
+                        <AddButton onClick={null}/>
                     </div>
                 </>
             )}
